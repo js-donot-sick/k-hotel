b3d01116f74c920eac0fe9b8ae31b62718aea05e
hojin1111 <jsung_9@naver.com>
2023-07-11 19:28:05 +0900

    2023-07-11

--- src/main/webapp/resources/js/signup.js
+++ src/main/webapp/resources/js/signup.js
@@ -1,3 +1,16 @@
+document.getElementsByClassName("J-H1-1")[0].addEventListener("click", function(){
+
+    const input = document.getElementsByClassName("J-J1-1")[0];
+
+    if(input.value == ""){
+        alert("아이디를 입력해주세요.");
+    } else {
+        alert("아직 구현되지 않은 기능입니다.");
+    }
+})
+
+
+
 document.getElementsByClassName("J-J1-1")[0].addEventListener("keydown", function(){
 
     const regExp = /^\w{6,12}$/;
@@ -49,10 +62,10 @@ document.getElementById("J-H3-1").addEventListener("click", function(){
     const pw1 = document.getElementsByClassName("J-J1-2")[0];
     const pw2 = document.getElementsByClassName("J-J1-3")[0];
 
-    if(pw1.value == pw2.value){
+    if(pw1.value == "" | pw2.value == ""){
+        alert("비밀번호를 입력해주세요.");
+    } else if(pw1.value == pw2.value){
         alert("비밀번호가 일치합니다.");
-    } else if(pw1.value == ""){
-        alert("비밀번호 입력 후 다시 눌러주세요.");
     } else{
         alert("비밀번호가 일치하지 않습니다.");
     }
