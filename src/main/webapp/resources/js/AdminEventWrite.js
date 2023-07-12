9055cb19bc3f57569e352933622151c7aca9eb7f
권호진 <khj981008@gmail.com>
2023-07-13 08:54:12 +0900

    2023-07-13 어드민 이벤트 수정 중

--- src/main/webapp/resources/js/AdminEventWrite.js
+++ src/main/webapp/resources/js/AdminEventWrite.js
@@ -6,11 +6,22 @@ $("#K-photo").on("change", function(){
         reader.onload = function (event) {
             var img = document.createElement("img");
             img.setAttribute("src", event.target.result);
-            $("img").addClass("K-photo-img");
+            img.setAttribute("width","auto;")
+            img.setAttribute("height","50px;")
+            img.setAttribute("style","margin-right: 10px;")
             document.querySelector("#K-image-select").appendChild(img);
         };
 
         console.log(image);
         reader.readAsDataURL(image);
+      }
+
+      
+      let input = $("#K-photo")[0].files;
+      
+      if(input.length>5){
+          alert("파일 첨부는 최대 5개만 가능합니다.");
+          $("#K-photo").val("");
+          /* $("#K-image-select").detach(); */ /* 악 수정 중 */
       }
 })
