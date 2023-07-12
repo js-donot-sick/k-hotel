f975bf3d6186748ebb14cbcbe91650d87de6c985
yuseong <lys8845@gmail.com>
2023-07-12 16:36:01 +0900

    2023-07-12 수정

--- src/main/webapp/resources/js/hotelinfo2.js
+++ src/main/webapp/resources/js/hotelinfo2.js
@@ -20,6 +20,79 @@ $("#L-star-1-1").on("mouseleave",function(){
     $("#L-room1").css("filter", "brightness(1)");
 })
 
-$("#")
+
+$("#L-star-1").on("change", function(){
+    
+    if(this.checked){
+        $("#L-star-1-1").css("color", "yellow").css("opacity", "0.9");
+
+    }else{
+        $("#L-star-1-1").css("color", "gray").css("opacity", "0.3");
+    }
+})
+
+// 방 2번 찜하기 버튼
+
+$("#L-room2").on("mouseenter",function(){
+    $("#L-star-2-1").css("display", "block");
+    $("#L-room2").css("filter", "brightness(0.7)");
+})
+
+$("#L-room2").on("mouseleave",function(){
+    $("#L-star-2-1").css("display", "none");
+    $("#L-room2").css("filter", "brightness(1)");
+})
+
+$("#L-star-2-1").on("mouseenter",function(){
+    $("#L-star-2-1").css("display", "block");
+    $("#L-room2").css("filter", "brightness(0.7)");
+})
+
+$("#L-star-2-1").on("mouseleave",function(){
+    $("#L-star-2-1").css("display", "none");
+    $("#L-room2").css("filter", "brightness(1)");
+})
 
 
+$("#L-star-2").on("change", function(){
+    
+    if(this.checked){
+        $("#L-star-2-1").css("color", "yellow").css("opacity", "0.9");
+
+    }else{
+        $("#L-star-2-1").css("color", "gray").css("opacity", "0.3");
+    }
+})
+
+// 방 3번 찜하기 버튼
+
+$("#L-room3").on("mouseenter",function(){
+    $("#L-star-3-1").css("display", "block");
+    $("#L-room3").css("filter", "brightness(0.7)");
+})
+
+$("#L-room3").on("mouseleave",function(){
+    $("#L-star-3-1").css("display", "none");
+    $("#L-room3").css("filter", "brightness(1)");
+})
+
+$("#L-star-3-1").on("mouseenter",function(){
+    $("#L-star-3-1").css("display", "block");
+    $("#L-room3").css("filter", "brightness(0.7)");
+})
+
+$("#L-star-3-1").on("mouseleave",function(){
+    $("#L-star-3-1").css("display", "none");
+    $("#L-room3").css("filter", "brightness(1)");
+})
+
+
+$("#L-star-3").on("change", function(){
+    
+    if(this.checked){
+        $("#L-star-3-1").css("color", "yellow").css("opacity", "0.9");
+
+    }else{
+        $("#L-star-3-1").css("color", "gray").css("opacity", "0.3");
+    }
+})
