2805a4e03d27590b2efe21bbd583c4b72680d57b
yonghyuk <choiyonghyuk97@gmail.com>
2023-07-21 08:17:30 +0900

    2023-07-21 카카오페이 연동 시도

--- src/main/webapp/resources/js/reservationFinal.js
+++ src/main/webapp/resources/js/reservationFinal.js
@@ -2,7 +2,7 @@ const agree = document.getElementById("C-revAgreeYes");
 const coupon = document.getElementById("C-rsv-coupon");
 const discount = document.getElementById("C-couponPay");
 const displaychanger = document.getElementById("C-display-change");
-const displaychanger2 =document.getElementById("C-display-change2");
+const displaychanger2 = document.getElementById("C-display-change2");
 const payplan = document.getElementById("C-payplan");
 const Cbank = document.getElementById("C-bank");
 const account = document.getElementById("C-account");
@@ -11,42 +11,61 @@ const account = document.getElementById("C-account");
 const hotelpay = document.getElementById("C-hotelPay");
 const finalpay = document.getElementById("C-finalpay");
 
-coupon.addEventListener("change", function(){
+coupon.addEventListener("change", function () {
 
-    if(coupon.value == "discountcoupon"){
+    if (coupon.value == "discountcoupon") {
         discount.value = "30000";
 
         finalpay.innerHTML = hotelpay.value - discount.value;
     }
 })
 
-payplan.addEventListener("change", function(){
+payplan.addEventListener("change", function () {
 
-    if(payplan.value=="무통장입금"){
+    if (payplan.value == "무통장입금") {
         displaychanger.style.display = "contents";
+    }
+
+    if (payplan.value == "카카오페이") {
+
     }
 })
 
-account.addEventListener("change", function(){
+account.addEventListener("change", function () {
 
-    if(account.value == "토스뱅크"){
+    if (account.value == "토스뱅크") {
         displaychanger2.style.display = "revert";
     }
 })
 
 function reservationFinalValidate() {
 
-   if (payplan.value != Cbank.value) {
-      alert("결제 수단을 선택해주시길 바랍니다.")
-      return false;
-   }
+    if (payplan.value != Cbank.value) {
+        alert("결제 수단을 선택해주시길 바랍니다.")
+        return false;
+    }
 
-   else if (!agree.checked) {
-      alert("약관 동의 후 예약을 진행해주시기 바랍니다.");
-      return false;
-   }
-   
- 
+    else if (!agree.checked) {
+        alert("약관 동의 후 예약을 진행해주시기 바랍니다.");
+        return false;
+    } else if (payplan.value == "카카오페이") {
+        $(function () {
+            $("#C-rsvClearBtn").click(function () {
+                $.ajax({
+                    url: '/cls/jp/kakaopay.cls',
+                    dataType: 'json',
+                    success: function (data) {
+                        alert(data);
+                    },
+                    error: function (error) {
+                        alert(error);
+                    }
+                })
+            })
+        })
+    }
 
-return confirm("결제를 진행하시겠습니까?");
+
+
+    return confirm("결제를 진행하시겠습니까?");
 }
