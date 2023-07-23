c9264bec77a2e1630ae61690f86fb9a9542da66e
pja <jnaacode@gmail.com>
2023-07-24 08:59:49 +0900

    2023-07-24 박진아 진행

---
+++ src/main/java/kHotel/member/model/service/BoardService.java
@@ -0,0 +1,19 @@
+package kHotel.member.model.service;
+
+import java.sql.Connection;
+
+import kHotel.member.model.vo.Board;
+
+public class BoardService {
+
+	public Board mypageWirte() {
+		
+		Connection conn = getco
+		
+		return null;
+	}
+
+
+
+	
+}
---
+++ src/main/java/kHotel/member/model/vo/Board.java
@@ -0,0 +1,20 @@
+package kHotel.member.model.vo;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Board {
+
+
+	private int boardNo;
+	private String boardTitle;
+	private String boardContent;
+	private String boardDt;
+
+}
