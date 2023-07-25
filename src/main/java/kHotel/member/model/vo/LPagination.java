f4fec1c8b65ee1d859f50561b66834ee8bdaf966
yuseong <lys8845@gmail.com>
2023-07-25 17:01:48 +0900

    2023-07-25 수정본

--- src/main/java/kHotel/member/model/vo/LPagination.java
+++ src/main/java/kHotel/member/model/vo/LPagination.java
@@ -21,8 +21,143 @@ public class LPagination {
 		this.currentPage = currentPage;
 		this.listCount = listCount;
 		
+		calculatePagination();
+		
+	}
+
+
+	public int getCurrentPage() {
+		return currentPage;
+	}
+
+
+	public void setCurrentPage(int currentPage) {
+		this.currentPage = currentPage;
+		
+		calculatePagination();
+	}
+
+
+	public int getListCount() {
+		return listCount;
+	}
+
+
+	public void setListCount(int listCount) {
+		this.listCount = listCount;
+		
+		calculatePagination();
+	}
+
+
+	public int getLimit() {
+		return limit;
+	}
+
+
+	public void setLimit(int limit) {
+		this.limit = limit;
+		
+		calculatePagination();
+	}
+
+
+	public int getPageSize() {
+		return pageSize;
+	}
+
+
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+		
+		calculatePagination();
+	}
+
+
+	public int getMaxPage() {
+		return maxPage;
+	}
+
+
+	public void setMaxPage(int maxPage) {
+		this.maxPage = maxPage;
+	}
+
+
+	public int getStartPage() {
+		return startPage;
+	}
+
+
+	public void setStartPage(int startPage) {
+		this.startPage = startPage;
+	}
+
+
+	public int getEndPage() {
+		return endPage;
+	}
+
+
+	public void setEndPage(int endPage) {
+		this.endPage = endPage;
+	}
+
+
+	public int getPrevPage() {
+		return prevPage;
+	}
+
+
+	public void setPrevPage(int prevPage) {
+		this.prevPage = prevPage;
+	}
+
+
+	public int getNextPage() {
+		return nextPage;
+	}
+
+
+	public void setNextPage(int nextPage) {
+		this.nextPage = nextPage;
+	}
+
+
+	@Override
+	public String toString() {
+		return "LPagination [currentPage=" + currentPage + ", listCount=" + listCount + ", limit=" + limit
+				+ ", pageSize=" + pageSize + ", maxPage=" + maxPage + ", startPage=" + startPage + ", endPage="
+				+ endPage + ", prevPage=" + prevPage + ", nextPage=" + nextPage + "]";
 	}
 	
+	
+	// 페이징 처리에 필요한 값을 계산하는 메소드
+	private void calculatePagination() {
+		
+		maxPage = (int)Math.ceil((double)listCount / limit);
+		
+		startPage = (currentPage - 1) / pageSize * pageSize + 1;
+		
+		endPage = startPage + pageSize - 1;
+		
+		if(endPage > maxPage) {
+			endPage = maxPage;
+		}
+		
+		if(currentPage <= pageSize) {
+			prevPage = 1;
+		}else {
+			prevPage = startPage -1;
+		}
+		
+		if(endPage == maxPage) {
+			nextPage = endPage;
+		}else {
+			nextPage = endPage+1;
+		}
+		
+	}
 	
 	
 	
