a00c7f9023e9418de788365be46031f8c1669b22
권호진 <khj981008@gmail.com>
2023-07-25 17:02:35 +0900

    2023-07-25 수업 중

---
+++ src/main/java/kHotel/board/controller/faqListServlet.java
@@ -0,0 +1,25 @@
+package kHotel.board.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/faq")
+public class faqListServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String path = "/WEB-INF/views/board/faqList.jsp";		
+		
+		RequestDispatcher dispatcher = req.getRequestDispatcher(path);
+		dispatcher.forward(req, resp);
+
+	}
+
+}
