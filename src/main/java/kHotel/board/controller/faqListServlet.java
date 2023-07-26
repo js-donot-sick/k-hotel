a90f2d5f75c5879c91467c9540f19f9dd82e30cd
권호진 <khj981008@gmail.com>
2023-07-26 19:10:14 +0900

    2023-07-26 보드 사이드바, qna, faq 수정 중

--- src/main/java/kHotel/board/controller/faqListServlet.java
+++ src/main/java/kHotel/board/controller/faqListServlet.java
@@ -1,6 +1,7 @@
 package kHotel.board.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +9,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import kHotel.board.model.service.KBoardService;
+import kHotel.board.model.vo.Board;
 
 @WebServlet("/board/faq")
 public class faqListServlet extends HttpServlet {
@@ -19,9 +23,16 @@ public class faqListServlet extends HttpServlet {
 			
 			int type = Integer.parseInt(req.getParameter("type"));
 			
+			KBoardService service = new KBoardService();
+			
+			List<Board> boardList = service.faqList(type);
+			
+			System.out.println(boardList);
+			
+			req.setAttribute("boardList", boardList);
+			
 		} catch (Exception e) {
-			
-			
+			e.printStackTrace();
 		}
 		
 		String path = "/WEB-INF/views/admin/AdminFaqList.jsp";		
