d47470b73042be24024b841d82aab167d13a484a
권호진 <khj981008@gmail.com>
2023-07-31 18:24:47 +0900

    2023-07-31 예약 완료페이지 진행 중

--- src/main/java/kHotel/member/controller/ReservationEndServlet.java
+++ src/main/java/kHotel/member/controller/ReservationEndServlet.java
@@ -8,6 +8,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import kHotel.member.model.vo.Member;
+import kHotel.member.model.vo.Reservation;
 
 @WebServlet("/book/bookEnd")
 public class ReservationEndServlet extends HttpServlet {
@@ -18,7 +22,35 @@ public class ReservationEndServlet extends HttpServlet {
 		
 		try {
 			
+			String hotelName = req.getParameter("hotelName");
+			String roomName = req.getParameter("roomName");
+			int bookPerson = Integer.parseInt(req.getParameter("bookPerson"));
+			int roomPrice = Integer.parseInt(req.getParameter("roomPrice")); // 할인 전 금액
+			int couponSale = Integer.parseInt(req.getParameter("couponSale"));
+			int hotelPay = Integer.parseInt(req.getParameter("C-hotelPay")); // 총 합계
+			
+			HttpSession session = req.getSession();
+			
+			Member loginMember = (Member)session.getAttribute("loginMember");
+			String memberName = loginMember.getMemberName();
+			String memberTel = loginMember.getMemberTel();
+			String memberEmail = loginMember.getMemberEmail();
+			
+			String payPlan = req.getParameter("C-payplan");
+			
+			Reservation reserve = new Reservation();
+			
+			reserve.setHotelName(hotelName);
+			reserve.setRoomName(roomName);
+			reserve.setBookPerson(bookPerson);
+			reserve.setRoomPrice(bookPerson);
+			reserve.setCouponSale(roomPrice);
+			reserve.setHotelPay(couponSale);
+			
+			
 		} catch (Exception e) {
+			
+			e.printStackTrace();
 			
 		}
 		
