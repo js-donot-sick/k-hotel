2ad99ec228035f4d86a03c62876218a7853dc84e
권호진 <khj981008@gmail.com>
2023-08-07 08:49:17 +0900

    2023-08-07 업다운, 비번 변경(로그인 전) 수정

--- src/main/webapp/resources/js/rerePw.js
+++ src/main/webapp/resources/js/rerePw.js
@@ -1,6 +1,6 @@
 const checkObj = {
-    "newPw"          : false,
-    "newPwConfirm"   : false,
+    "newPw": false,
+    "newPwConfirm": false,
 };
 
 // 비밀번호 유효성 검사
@@ -8,9 +8,9 @@ const newPw = document.getElementById("newPw");
 const newPwConfirm = document.getElementById("newPwConfirm");
 const message = document.getElementById("k-message");
 
-newPw.addEventListener("input", function(){
-    if(newPw.value.trim().length == 0){
-        message.innerText = "영어, 숫자, 특수문자,(!,@,#,-,_) 6~14글자 사이로 작성해주세요.";
+newPw.addEventListener("input", function () {
+    if (newPw.value.trim().length == 0) {
+        message.innerText = "영어, 숫자, 특수문자,(!,@,#,-,_) 8~15글자 사이로 작성해주세요.";
         message.classList.remove("k-confirm", "k-error");
         checkObj.newPw = false;  // 유효 X 기록
         return;
@@ -18,22 +18,41 @@ newPw.addEventListener("input", function(){
 
     const regExp = /^([A-Z]|[a-z]|[0-9]|[~\!\@\$]){8,15}$/;
 
-    if(regExp.test(newPw.value)){ 
-        
-        checkObj.newPw = true; 
+    if (regExp.test(newPw.value)) {
 
-        if(newPwConfirm.value.trim().length ==0){ 
+        checkObj.newPw = true;
+
+        if (newPwConfirm.value.trim().length == 0) {
 
             message.innerText = "유효한 비밀번호 형식입니다.";
             message.classList.add("k-confirm");
             message.classList.remove("k-error");
 
-        } else { 
-            checkPw(); 
+            newPwConfirm.addEventListener("input", checkPw)
+
+            function checkPw() { // 비밀번호 일치 검사
+
+                // 비밀번호 / 비밀번호 확인이 같을 경우
+                if (newPw.value == newPwConfirm.value) {
+                    message.innerText = "비밀번호가 일치합니다.";
+                    message.classList.add("k-confirm");
+                    message.classList.remove("k-error");
+                    checkObj.newPwConfirm = true;  // 유효 O 기록
+
+                } else {
+                    message.innerText = "비밀번호가 일치하지 않습니다.";
+                    message.classList.add("k-error");
+                    message.classList.remove("k-confirm");
+                    checkObj.newPwConfirm = false;  // 유효 X 기록
+                }
+            }
+
+        } else {
+            checkPw();
         }
 
 
-    } else{
+    } else {
         message.innerText = "비밀번호 형식이 유효하지 않습니다.";
         message.classList.add("k-error");
         message.classList.remove("k-confirm");
@@ -41,36 +60,19 @@ newPw.addEventListener("input", function(){
     }
 })
 
-newPwConfirm.addEventListener("input", checkPw)
 
-function checkPw(){ // 비밀번호 일치 검사
-    
-      // 비밀번호 / 비밀번호 확인이 같을 경우
-      if(newPw.value == newPwConfirm.value){ 
-        message.innerText = "비밀번호가 일치합니다.";
-        message.classList.add("k-confirm");
-        message.classList.remove("k-error");
-        checkObj.newPwConfirm = true;  // 유효 O 기록
 
-    } else{
-        message.innerText = "비밀번호가 일치하지 않습니다.";
-        message.classList.add("k-error");
-        message.classList.remove("k-confirm");
-        checkObj.newPwConfirm = false;  // 유효 X 기록
-    }
-}
-
-function pwValidate(){
+function pwValidate() {
 
     let str;
 
-    for(let key in checkObj){
+    for (let key in checkObj) {
 
-        if(!checkObj[key]){
+        if (!checkObj[key]) {
 
-            switch(key){
-                case "newPw"          : str="비밀번호가 "; break;
-                case "newPwConfirm"   : str="비밀번호 확인이 "; break;
+            switch (key) {
+                case "newPw": str = "비밀번호가 "; break;
+                case "newPwConfirm": str = "비밀번호 확인이 "; break;
             }
 
             str += "유효하지 않습니다.";
@@ -80,5 +82,5 @@ function pwValidate(){
             return false;
         }
     }
-    return true; 
+    return true;
 }
